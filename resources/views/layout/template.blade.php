4a7c23c803b7e12842bd98ffafbcfad3a45b21d8
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-23 21:21:04 +0700

    upload resources

---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title><?= $title; ?></title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link rel="stylesheet" href="/css/styles.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/bootstrap.css" />
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/font-awesome/font-awesome.css" />
+    <link rel="stylesheet" href="/font-awesome/font-awesome.min.css" />
+  </head>
+  <body>
+
+<?= $this->include('layout/navbar'); ?>
+
+<?= $this->renderSection('content'); ?>
+
+  <div class="footer_agileinfo_w3">
+      <div class="footer_inner_info_w3ls_agileits">
+        <div class="row">
+          <div class="col-md-4">
+            <h2>
+              <a href="/">
+                <img src="/img/logo ciptajayalestari.PNG" style="margin-top: -10px" width="100px" />
+              </a>
+            </h2>
+            <h4 style="color: red"><b>Metal working and machinery</b></h4>
+          </div>
+          <div class="col-md-4">
+            <div class="sign-grds">
+              <div class="col-md-5 sign-gd-one">
+                <h4>Hubungi <span>Kami</span></h4>
+                <div class="address">
+                  <div class="address-grid">
+                    <div class="address-right">
+                      <h6>Layanan Pelanggan</h6>
+                      <i class="fa fa-whatsapp" aria-hidden="true"></i>
+                      <div class="phone-domain">
+                        <a href="tel:0313896920" class="nobor">0313896920</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+                <br>
+                <div class="address-grid">
+                  <div class="address-right">
+                    <h6>E-mail</h6>
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <p><a href="mailto:ciptajayalestari@gmail.com" style="color: white">ciptajayalestari@gmail.com</a></p>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+                <br>
+                <div class="address-grid">
+                        <div class="address-right">
+                          <h6>Alamat</h6> 
+                          <p><i class="bi bi-house-door"></i>   Jalan Kenjeran 300 Blok A-7 Surabaya, Jawa Timur, indonesia</p>
+                        </div>
+                      <div class="clearfix"></div>
+                    </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">             
+          <div class="sign-gd-two emailprofile cta-footer text-center" id="wa_company" data-wa="6281335700084;6281330066919;628123170863;6281254693810;6281331637497;085232777499" data-company="//www.ciptajayalestari.com" data-member="2">
+            <div id="wa">
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Sales Counter</b></p>
+                  <img src="/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281335700084&amp;text=Halo Sales Counter, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081335700084</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Edi</b></p>
+                  <img src="/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=628123170863&amp;text=Halo Pak Edi, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">08123170863</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Ilyas</b></p>
+                  <img src="/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281254693810&amp;text=Halo Pak Ilyas, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081254693810</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Rochman</b></p>
+                  <img src="/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6281331637497&amp;text=Halo Pak Rochman, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">081331637497</span></a>
+                </div>
+              </div>
+              <div class="phone-domain">
+                <div style="padding: 6px 10px">
+                  <p><b>Pak Dwiono</b></p>
+                  <img src="/img/whatsapp.png" alt="" style="margin-right: 5px; height: 30px; width: 30px" /><span class="" style="float: left; background-color: #25d366"></span>
+                  <a target="_blank" href="https://api.whatsapp.com/send?phone=6285232777499&amp;text=Halo Pak Dwiono, Saya dapat nomor dari www.ciptajayalestari.id" class="nobor" style="font-size: 18px">
+                    <span class="hidden-md-down" style="color: #fff">085232777499</span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+              <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <script>
+      // Can also be used with $(document).ready()
+      $(document).ready(function () {
+        $(".flexslider").flexslider({
+          animation: "slide",
+          controlNav: "thumbnails",
+        });
+      });
+    </script>
+  </body>
+</html>

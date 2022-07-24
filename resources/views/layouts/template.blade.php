6ae44852edb776e17f78020a0443525d156cede0
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-24 14:36:34 +0700

    ganti title

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title><?= $title; ?></title>
+    <title>CIPTA JAYA LESTARI</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
     <!-- Bootstrap icons-->

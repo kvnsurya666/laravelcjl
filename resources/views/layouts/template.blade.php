a41f556f76cdced82af7e073f8a8fdb1402c5f16
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-26 15:14:57 +0700

    change link tentang kami

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -24,8 +24,8 @@
     <nav class="navbar navbar-expand-lg sticky-top bg-light">
       <div class="container">
                  
-            <a class="navbar-brand" href="#" style="padding-top: 10px; padding-bottom: 10px">
-              <img src="/assets/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
+            <a class="navbar-brand" href="/" style="padding-top: 10px; padding-bottom: 10px">
+              <img src="/img/logo ciptajayalestari.PNG" alt="" width="100" height="50" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -51,7 +51,7 @@
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="tentangkami.php">About Us</a>
+                <a class="nav-link" href="/tentangkami">About Us</a>
               </li>
             </ul>
           </div>

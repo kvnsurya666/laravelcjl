45ece334cff34abe4262671ae375dcc1db80fb63
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-26 17:37:24 +0700

    tambah view login

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', function () {
-    return view('/admin/home');
+    return view('/admin/login');
 });
 
 Route::get('/mesin', function () {

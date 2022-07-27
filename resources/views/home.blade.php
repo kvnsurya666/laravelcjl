6e9fb68fa3e6dfb3d87030e6a47a465a98d8c287
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-27 13:18:02 +0700

    revert images home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
       <div id="carouselSlide" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="/assets/img/slider1.jpg" class="d-block w-100" alt="Gambar 1" />
+            <img src="/img/slider1.jpg" class="d-block w-100" alt="Gambar 1" />
           </div>
           <div class="carousel-item">
-            <img src="/assets/img/slider2.jpg" class="d-block w-100" alt="Gambar 2" />
+            <img src="/img/slider2.jpg" class="d-block w-100" alt="Gambar 2" />
           </div>
           <div class="carousel-item">
-            <img src="/assets/img/slider3.jpg" class="d-block w-100" alt="Gambar 3" />
+            <img src="/img/slider3.jpg" class="d-block w-100" alt="Gambar 3" />
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselSlide" data-bs-slide="prev">
@@ -43,22 +43,22 @@
                   <div class="row justify-content-center" style="padding: 20px;">
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
                       <a href="mesin.php">
-                        <img src="/assets/img/Logo/weida.png" class="card-img-top" alt="WEIDA">
+                        <img src="/img/Logo/weida.png" class="card-img-top" alt="WEIDA">
                       </a> 
                       
                     </div>
 
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/dixon.jpg" class="card-img-top" alt="DIXON">
+                        <img src="/img/Logo/dixon.jpg" class="card-img-top" alt="DIXON">
                       
                     </div>
               
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/fullmark.jpg" class="card-img-top" alt="FULLMARK">
+                        <img src="/img/Logo/fullmark.jpg" class="card-img-top" alt="FULLMARK">
                       
                     </div>
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/bositools.png" class="card-img-top" alt="BOSI TOOLS">
+                        <img src="/img/Logo/bositools.png" class="card-img-top" alt="BOSI TOOLS">
                       
                     </div>
                 </div>
@@ -66,22 +66,22 @@
                 <div class="carousel-item">
                   <div class="row justify-content-center" style="padding: 20px;">
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/J&W.jpg" class="card-img-top" alt="J&W">
+                        <img src="/img/Logo/J&W.jpg" class="card-img-top" alt="J&W">
                       
                     </div>
 
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/Besdia.jpg" class="card-img-top" alt="Besdia">
+                        <img src="/img/Logo/Besdia.jpg" class="card-img-top" alt="Besdia">
                       
                     </div>
 
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/Carmar.jpg" class="card-img-top" alt="CarMar">
+                        <img src="/img/Logo/Carmar.jpg" class="card-img-top" alt="CarMar">
                       
                     </div>
 
                     <div class="card col-md-4" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/Arntz.jpg" class="card-img-top" alt="Arntz">
+                        <img src="/img/Logo/Arntz.jpg" class="card-img-top" alt="Arntz">
                       
                     </div>
                 </div>
@@ -89,22 +89,22 @@
                 <div class="carousel-item">
                   <div class="row justify-content-center" style="padding: 20px;">
                     <div class="card col-md-3" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/vertex.png" class="card-img-top" alt="VERTEX">
+                        <img src="/img/Logo/vertex.png" class="card-img-top" alt="VERTEX">
                       
                     </div>
 
                     <div class="card col-md-3" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/seayac.jpg" class="card-img-top" alt="SEAYAC">
+                        <img src="/img/Logo/seayac.jpg" class="card-img-top" alt="SEAYAC">
                       
                     </div>
 
                     <div class="card col-md-3" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/waytrain.jpg" class="card-img-top" alt="WAYTRAIN">
+                        <img src="/img/Logo/waytrain.jpg" class="card-img-top" alt="WAYTRAIN">
                       
                     </div>
 
                     <div class="card col-md-3" style="width: 20rem; padding-right: 10px;">
-                        <img src="/assets/img/Logo/sanou.jpg" class="card-img-top" alt="SAN OU">
+                        <img src="/img/Logo/sanou.jpg" class="card-img-top" alt="SAN OU">
                       
                     </div>
                 </div>
@@ -125,42 +125,42 @@
     <section class="showcase">
       <div class="container-fluid p-0">
         <div class="row g-0">
-          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/assets/img/gambar1.jpg')"></div>
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/img/gambar1.jpg')"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
             <h3>Machinery</h3>
             <p>Ingin cari macam - macam mesin?</p>
             <div class="shop-button">
-              <a href="mesin.php">Lihat Disini</a>
+              <a href="/mesin">Lihat Disini</a>
             </div>
           </div>
         </div>
         <div class="row g-0">
-          <div class="col-lg-6 text-white showcase-img" style="background-image: url('/assets/img/gambar2.jpg')"></div>
+          <div class="col-lg-6 text-white showcase-img" style="background-image: url('/img/gambar2.jpg')"></div>
           <div class="col-lg-6 my-auto showcase-text">
             <h3>Machine Tools</h3>
             <p>Ingin cari berbagai machine tool?</p>
             <div class="shop-button">
-              <a href="machinetool.php">Lihat Disini</a>
+              <a href="/machinetool">Lihat Disini</a>
             </div>
           </div>
         </div>
         <div class="row g-0">
-          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/assets/img/gambar3.jpg')"></div>
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('/img/gambar3.jpg')"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
             <h3>Accesories</h3>
             <p>Ingin cari macam - macam aksesoris?</p>
             <div class="shop-button">
-              <a href="aksesoris.php">Lihat Disini</a>
+              <a href="/aksesoris">Lihat Disini</a>
             </div>
           </div>
         </div>
         <div class="row g-0">
-          <div class="col-lg-6 text-white showcase-img" style="background-image: url('/assets/img/jasa.jpg')"></div>
+          <div class="col-lg-6 text-white showcase-img" style="background-image: url('/img/jasa.jpg')"></div>
           <div class="col-lg-6 my-auto showcase-text">
             <h3>Cutting Tools</h3>
             <p>Ingin cari berbagai jenis cutting tool?</p>
             <div class="shop-button">
-              <a href="cuttingtool.php">Lihat Disini</a>
+              <a href="/cuttingtool">Lihat Disini</a>
             </div>
           </div>
         </div>

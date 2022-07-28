5422e137263bc8fc7d3cd94d29a0861d2253194a
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-07-28 13:42:56 +0700

    sales table

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,13 +15,29 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('admin')->insert([
-            'id_admin' => '1234',
+        DB::table('admin')->insert([
+            'id_admin' => '1',
             'nama' => 'Kevin',
             'role' => 'admin',
             'email' => 'kevinsuryaperdana@gmail.com',
             'username' => 'kevinganteng', 
             'password' => ('kevin211096')
+        ]);
+        DB::table('admin')->insert([
+            'id_admin' => '2',
+            'nama' => 'Fenny',
+            'role' => 'admin',
+            'email' => 'fennyresav18@gmail.com',
+            'username' => 'fennyresav18', 
+            'password' => ('fenny')
+        ]);
+        DB::table('admin')->insert([
+            'id_admin' => '3',
+            'nama' => 'Ilyas',
+            'role' => 'sales',
+            'email' => 'ilyas@gmail.com',
+            'username' => 'ilyas', 
+            'password' => ('ilyas')
         ]);
     }
 }
--- database/seeders/ModelSalesSeeder.php
+++ database/seeders/ModelSalesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ModelSalesSeeder extends Seeder
 {
@@ -14,6 +15,15 @@ class ModelSalesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('sales')->insert([
+            'nama' => 'Andi Saputra',
+            'nis' => 101,
+            'tgl_lahir' => '2003-12-22',
+        ]);
+        DB::table('sales')->insert([
+            'nama' => 'Budi Cahya',
+            'nis' => 102,
+            'tgl_lahir' => '2004-01-30',
+        ]);
     }
 }
